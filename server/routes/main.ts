71d39cb8f9a6da4557b59a8670bd87bfab0a2de5
zander <akrog13@student.aau.dk>
2016-09-23 12:54:23 +0200

    tumblr

--- server/routes/main.ts
+++ server/routes/main.ts
@@ -2,7 +2,35 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const mainRouter: Router = Router();
 var counter = 0;
-//error handling for unknown map files.
+
+var apiKey = 'GUfecC9FtcRV24UaIFUdV3jZnXUjkSMOkCDAIGEp3GHPDnrJEA';
+
+// Authenticate via OAuth
+var tumblr = require('tumblr.js');
+var client = tumblr.createClient({'credentials': {
+    consumer_key: 'GUfecC9FtcRV24UaIFUdV3jZnXUjkSMOkCDAIGEp3GHPDnrJEA',
+    consumer_secret: 'F8AjTRAQvxmiB5hmi1XoSvuQs1W0vNT4iq65YtMdmblN1Pe3kd',
+    token: 's0W9SslyMDCe3UZvcsXqPRCYP0BMrpxLHdMe5Dq1Fa5rOdTYpJ',
+    token_secret: '40DuCivpQM15FSJEKBpK716FdrEE0PakfL1EtWk9dbmoTQ8G4f',
+    },
+    returnPromises: true
+});
+
+client.returnPromises();
+
+mainRouter.get("/tumblr", function(req, res, next) {
+
+    let test = client.blogPosts("heck-yeah-old-tech");
+    test.then((data) => {
+
+
+        res.json(data.posts.filter((x) => (x.type == "photo" )  ));
+    })
+    
+});
+
+
+
 mainRouter.get("/assets/js/vendor/*.map", function (request: Request, response: Response, next: NextFunction) {
     let err = new Error("Not Found");
     next(err);
